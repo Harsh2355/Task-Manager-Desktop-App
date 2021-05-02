14df2aa020e7aef842a1d1f1aaead744899109e6
Harshvardhan <harshvardhankakar@gmail.com>
2021-05-02 15:41:21 +0530

    =modified electron.js

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -18,7 +18,7 @@ function createWindow() {
 
     // and load the index.html of the app.
     //mainWindow.loadURL('http://localhost:3000');
-    mainWindow.loadURL('placeholder');
+    mainWindow.loadURL('https://yourtaskbuddy-hk.herokuapp.com');
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
